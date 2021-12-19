bd11082e53e9e2e2dff19290f7f63ceedc6e882c
Mani5871 <manideepak650@gmail.com>
2021-12-19 13:44:12 +0530

    DOM

---
+++ DAY 03/Window.js
@@ -0,0 +1,22 @@
+console.log("Window Object")
+
+let obj = window;
+console.log(obj);
+
+// alert("Hello");
+
+my_name = prompt("Enter your name");
+console.log(my_name);
+
+message = confirm("Are you sure you want to continue?");
+console.log(message);
+
+width = window.innerWidth;
+height = window.innerHeight;
+
+console.log(width + "x" + height);
+
+// Scrollx and scrolly are used to find out the relative position of the window while scrolling
+console.log(history)
+
+// Some more methods are location, history, 
